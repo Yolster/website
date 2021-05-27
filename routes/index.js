d4eddbadda2369cf7fc432d8d7eb648f52d34a47
Yolster <yolster@protonmail.com>
2021-05-27 19:00:34 +0300

    Update codes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+var settings = require('../settings.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  request({
+    url: `https://discordapp.com/api/v7/users/${settings.id}`,
+    headers: {
+    "Authorization": `Bot ${settings.token}`
+    },
+    }, async(error, response, body) => {
+    if (error) return console.log(error)
+    else if (!error) {
+    var user = JSON.parse(body)
+      res.render('index', {
+        user:user,
+        font: settings.fontawesome
+      })
+    }
+})
 });
 
 module.exports = router;
